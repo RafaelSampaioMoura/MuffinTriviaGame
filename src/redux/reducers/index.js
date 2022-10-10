cc390a07ed77fc32f60276b4884764dbdeb7ad8b
HailMuffins <rafaelsm123@outlook.com>
2022-10-10 14:48:19 -0300

    Salvo Informações Login

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,39 +1,51 @@
-import { combineReducers } from 'redux';
-import { FAILURE_TOKEN, REQUEST_TOKEN, SUCCESS_TOKEN } from '../actions';
-import INITIAL_STATE from './initialState';
+import { combineReducers } from "redux";
+import md5 from "crypto-js/md5";
+import {
+  FAILURE_TOKEN,
+  REQUEST_TOKEN,
+  SUCCESS_TOKEN,
+  SUBMIT_PLAYER_INFO,
+} from "../actions";
+import INITIAL_STATE from "./initialState";
 
-const nameReducer = (state = INITIAL_STATE.player, action) => {
+const playerInfoReducer = (state = INITIAL_STATE.player, action) => {
   switch (action.type) {
-  default:
-    return state;
+    case SUBMIT_PLAYER_INFO:
+      return {
+        ...state,
+        name: action.payload.playerName,
+        gravatarEmail: md5(action.payload.playerEmail).toString(),
+      };
+    default:
+      return state;
   }
 };
 
 const tokenReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case REQUEST_TOKEN:
-    return {
-      ...state,
-      isFetching: true,
-    };
-  case SUCCESS_TOKEN:
-    return {
-      ...state,
-      token: action.payload.token,
-      responseCode: action.payload.response_code,
-      isFetching: false,
-    };
-  case FAILURE_TOKEN:
-    return {
-      ...state,
-      error: action.error,
-      isFetching: false,
-    };
-  default:
-    return state;
+    case REQUEST_TOKEN:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case SUCCESS_TOKEN:
+      return {
+        ...state,
+        token: action.payload.token,
+        responseCode: action.payload.response_code,
+        isFetching: false,
+      };
+    case FAILURE_TOKEN:
+      return {
+        ...state,
+        error: action.error,
+        isFetching: false,
+      };
+    default:
+      return state;
   }
 };
 
-const rootReducer = combineReducers({ nameReducer, tokenReducer });
+const rootReducer = combineReducers({ playerInfoReducer, tokenReducer });
 
 export default rootReducer;
